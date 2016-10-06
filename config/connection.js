603cc004dce6b1f69b93d2ec0c0ab50c9a42e202
Katie Witham <kewitham@gmail.com>
2016-10-06 15:59:19 -0700

    port

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
 connection = mysql.createConnection({
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 3306,
 	host: 'localhost',
 	user: 'root',
 	password: '',
